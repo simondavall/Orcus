b2092a5df5052f19890103c71169dcc680590196
Simon Da Vall <simon.davall@gmail.com>
2024-07-22 18:43:00 +0100

    Added encrytion process

--- encrypt/encrypt.c
+++ encrypt/encrypt.c
@@ -1,7 +1,10 @@
+#include <sodium/crypto_secretstream_xchacha20poly1305.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <sodium.h>
+
+const int CHUNK_SIZE = 4096;
 
 const int MAX_FILEPATH_LENGTH = 128;
 const int MAX_PASSWORD_LENGTH = 20;
@@ -14,6 +17,7 @@ bool checkPasswordValidChars(const char* password);
 bool checkValid(char* validChars, const char* toBeChecked, char* messageLabel);
 bool checkValidFile(const char* filepath);
 bool encryptFile(const char* filepath, const char* password);
+static bool encrypt(const char *targetFile, const char *sourceFile, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]);
 bool GenerateSecretKey(unsigned char* const key, unsigned long long keyLen, const char* password);
 void ReadBytes(char *bytesRead, int numBytesRead, FILE *file, const int length);
 
@@ -57,7 +61,7 @@ int main(int argc, char* argv[]){
   //todo sdv all tests passed so now encrypt the file.
   //https://stackoverflow.com/questions/7622617/simply-encrypt-a-string-in-c
   //have a look at this link
-  //todo sdv change the password into a secret key and zero the password before sending
+//todo sdv change the password into a secret key and zero the password before sending
   if(!encryptFile(filepath, password)){
     return 1;
   }
@@ -191,71 +195,40 @@ bool checkValidFile(const char* filepath){
   return true;
 }
 
-bool encryptFile(const char* filepath, const char* password){
-  FILE * fptr;
-  fptr = fopen(filepath, "r");
-  int length = 512;
-  char bytesRead[length];
+static bool 
+encrypt(const char *targetFile, const char *sourceFile, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]){
 
-  int numBytesRead = length;
-
-  while(numBytesRead == length){
-    // reset counter
-    numBytesRead = 0;
-    // zero the bytes read array
-    for (int i = 0; i < length; i++) {
-      bytesRead[i] = '\0';
-    }
-    // read the bytes from file
-    ReadBytes(bytesRead, numBytesRead, fptr, length);
-
-    // set the bytes up for encryption
-    unsigned char message = bytesRead[numBytesRead];
-    int cipherTextLen = numBytesRead + crypto_secretstream_xchacha20poly1305_ABYTES;
-    unsigned char cipher[cipherTextLen];
-
-    // todo: sdv Step 1: need to encrypt this message and save it to file
-    // Step 2: need to deal with larger files, i.e. loop until number of bytes read is zero.
-    // Then add the last message crypto tag to eht final message and save the loops to file.
-    // May be able to save the progress to file any way.
-    // Edge case: The last read get all the remaining bytes and numBytesRead == length.
-
-  }
- 
-  printf("The file contents were:\n");
-  int current = 0;
-  while(bytesRead[current] != '\0'){
-    printf("%c", bytesRead[current]);
-    current++;
-
-    if (current > length){
-      printf("Error printing file contents.");
-      break;
-    }
-  }
-  printf("\nNumber of chars printed: %d\n", current);
-
-
-
-
-  #define MESSAGE_PART1 (const unsigned char *) "Arbitrary data to encrypt"
-  #define MESSAGE_PART1_LEN    25
-  #define CIPHERTEXT_PART1_LEN MESSAGE_PART1_LEN + crypto_secretstream_xchacha20poly1305_ABYTES
-
-  #define MESSAGE_PART2 (const unsigned char *) "split into"
-  #define MESSAGE_PART2_LEN    10
-  #define CIPHERTEXT_PART2_LEN MESSAGE_PART2_LEN + crypto_secretstream_xchacha20poly1305_ABYTES
-
-  #define MESSAGE_PART3 (const unsigned char *) "three messages"
-  #define MESSAGE_PART3_LEN    14
-  #define CIPHERTEXT_PART3_LEN MESSAGE_PART3_LEN + crypto_secretstream_xchacha20poly1305_ABYTES
-
-  crypto_secretstream_xchacha20poly1305_state state;
+  unsigned char buf_in[CHUNK_SIZE];
+  unsigned char buf_out[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
   unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
-  unsigned char c1[CIPHERTEXT_PART1_LEN],
-                c2[CIPHERTEXT_PART2_LEN],
-                c3[CIPHERTEXT_PART3_LEN];
- 
+  crypto_secretstream_xchacha20poly1305_state state;
+  FILE *fptr_s, *fptr_t;
+  unsigned long long out_len;
+  size_t rlen;
+  int eof;
+  unsigned char tag;
+
+  fptr_s = fopen(sourceFile, "rb");
+  fptr_t = fopen(targetFile, "wb");
+
+  crypto_secretstream_xchacha20poly1305_init_push(&state, header, key);
+  fwrite(header, 1, sizeof header, fptr_t);
+  do{
+    rlen = fread(buf_in, 1, sizeof buf_in, fptr_s);
+    eof = feof(fptr_s);
+    tag = eof ? crypto_secretstream_xchacha20poly1305_TAG_FINAL : 0;
+    crypto_secretstream_xchacha20poly1305_push(&state, buf_out, &out_len, buf_in, rlen, NULL, 0, tag);
+    fwrite(buf_out, 1, (size_t) out_len, fptr_t);
+  } while(!eof);
+  fclose(fptr_t);
+  fclose(fptr_s);
+
+  return true;
+
+}
+
+bool encryptFile(const char *filepath, const char *password){
+
   /* Shared secret key required to encrypt/decrypt the stream */
   unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
 
@@ -264,27 +237,18 @@ bool encryptFile(const char* filepath, const char* password){
     return false;
   }
 
-  /* Set up a new stream: initialize the state and create the header */
-  crypto_secretstream_xchacha20poly1305_init_push(&state, header, key);
+  char targetFilepath[strlen(filepath) + 4];
+  strcpy(targetFilepath, filepath);
+  strcat(targetFilepath, ".enc");
 
-  /* Now, encrypt the first chunk. `c1` will contain an encrypted,
-   * authenticated representation of `MESSAGE_PART1`. */
-  crypto_secretstream_xchacha20poly1305_push
-   (&state, c1, NULL, MESSAGE_PART1, MESSAGE_PART1_LEN, NULL, 0, 0);
+  // create or open/truncate the file to hold encrypted data 
+  FILE *encryptedfptr;
+  encryptedfptr = fopen(targetFilepath, "w");
+  fclose(encryptedfptr);
 
-  /* Encrypt the second chunk. `c2` will contain an encrypted, authenticated
-   * representation of `MESSAGE_PART2`. */
-  crypto_secretstream_xchacha20poly1305_push
-   (&state, c2, NULL, MESSAGE_PART2, MESSAGE_PART2_LEN, NULL, 0, 0);
-
-  /* Encrypt the last chunk, and store the ciphertext into `c3`.
-   * Note the `TAG_FINAL` tag to indicate that this is the final chunk. */
-  crypto_secretstream_xchacha20poly1305_push
-   (&state, c3, NULL, MESSAGE_PART3, MESSAGE_PART3_LEN, NULL, 0,
-    crypto_secretstream_xchacha20poly1305_TAG_FINAL);
-
-  printf("Reached the end of the encryption.\n");
-  return true;
+  printf("Set up files and ready to encrypt");
+  //return true;
+  return encrypt(targetFilepath, filepath, key);
 }
 
 bool GenerateSecretKey(unsigned char* const out, unsigned long long outLen, const char* password){
@@ -303,25 +267,4 @@ bool GenerateSecretKey(unsigned char* const out, unsigned long long outLen, cons
   }
 
   return true;
-}
-
-void ReadBytes(char *bytesRead, int numBytesRead, FILE *file, const int length)
-{
-  int numBytesToRead = length;
-  int check = 0;
-  while (numBytesToRead > 0) {
-    size_t n = fread(bytesRead, sizeof(int) , length, file);
-    if (n == 0) break;
-
-    numBytesRead += n;
-    numBytesToRead -= n;
-
-    if (check++ > 3){
-      // more than three attempts to read all bytes
-      printf("Error reading from file");
-      return;
-    }
-  }
-
-  return;
 }
