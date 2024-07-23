ad31f11eb1b72a1a929e81790a645d296b719b04
Simon Da Vall <simon.davall@gmail.com>
2024-07-23 16:20:56 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-gcc -o build/encrypt encrypt/encrypt.c -lsodium
+gcc -c -g -o encrypt/socrates/validation.o encrypt/socrates/validation.c
+gcc -c -g -o encrypt/socrates/encryption.o encrypt/socrates/encryption.c
+
+gcc -o build/encrypt encrypt/encrypt.c -lsodium encrypt/socrates/validation.o encrypt/socrates/encryption.o
+gcc -o build/decrypt encrypt/decrypt.c -lsodium encrypt/socrates/validation.o encrypt/socrates/encryption.o
 
 cd build
