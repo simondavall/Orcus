72e74767880929f86992f1586b61b32d6192adf0
Simon Da Vall <simon.davall@gmail.com>
2024-07-24 13:44:55 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+BASEDIR=$(cd $(dirname $0) && pwd)
 
-gcc -c -g -o encrypt/socrates/validation.o encrypt/socrates/validation.c
-gcc -c -g -o encrypt/socrates/encryption.o encrypt/socrates/encryption.c
+SRC_DIR=encrypt
+SOC_LIB=$SRC_DIR/socrates
 
-gcc -o build/encrypt encrypt/encrypt.c -lsodium encrypt/socrates/validation.o encrypt/socrates/encryption.o
-gcc -o build/decrypt encrypt/decrypt.c -lsodium encrypt/socrates/validation.o encrypt/socrates/encryption.o
+
+gcc -c -g -o $SOC_LIB/validation.o $SOC_LIB/validation.c
+gcc -c -g -o $SOC_LIB/encryption.o $SOC_LIB/encryption.c
+
+gcc -o build/encrypt $SRC_DIR/encrypt.c -lsodium $SOC_LIB/validation.o $SOC_LIB/encryption.o
+gcc -o build/decrypt $SRC_DIR/decrypt.c -lsodium $SOC_LIB/validation.o $SOC_LIB/encryption.o
 
 cd build
